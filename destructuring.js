e5cd1cf5a3dbcfbd0add8aa5a1dc0546e8157e48
IainJackson90 <>
2023-12-21 20:12:30 +0000

    Add: destructuring

--- destructuring.js
+++ destructuring.js
@@ -5,8 +5,37 @@
 
 // Destructuring arrays
 
+let ages = [30, 26, 27];
+// let jhon = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [jhon, mary, joe] = ages;
+console.log(jhon, mary, joe);
+
 // Destructuring objects
+
+let jobs = {
+    mike: "desingner",
+    jill: "developer",
+    alicia: "accountant"
+};
+let { mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
 
-// Using rest parameter syntax
+let fruits = ["apples", "orange", "banana", "peach", "cherry"];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegan",
+    andrea: "steak"
+};
+let {brian, anna, ...rest} = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
